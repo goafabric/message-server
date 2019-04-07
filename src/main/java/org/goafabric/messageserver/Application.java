3d91243912cf1bbb0aaee164907dd57c6dbf4cc7
goafabric <amautsch@gmx.de>
2019-04-07 21:34:54 +0200

    initial import

---
+++ src/main/java/org/goafabric/messageserver/Application.java
@@ -0,0 +1,13 @@
+
+package org.goafabric.messageserver;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jms.annotation.EnableJms;
+
+@SpringBootApplication
+@EnableJms
+public class Application {
+    public static void main(String[] args) {
+    }
+
+}
---
+++ src/main/java/org/goafabric/messageserver/logic/Receiver.java
@@ -0,0 +1,17 @@
+package org.goafabric.messageserver.logic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.goafabric.messageserver.Email;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class Receiver {
+
+    @JmsListener(destination = "mailbox", containerFactory = "myFactory")
+    public void receiveMessage(Email email) {
+        log.info("Received <" + email + ">");
+    }
+
+}
