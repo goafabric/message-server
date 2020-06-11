3425eb790c1c4f11c0031802b488e4ca1c5b193b
Mautsch, Andreas <andreas.mautsch@cgm.com>
2020-06-11 11:36:40 +0200

    refactoring

--- message-server/src/main/java/org/goafabric/messageserver/configuration/JmsConfiguration.java
+++ message-server/src/main/java/org/goafabric/messageserver/configuration/JmsConfiguration.java
@@ -3,6 +3,7 @@ package org.goafabric.messageserver.configuration;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,20 +13,29 @@ import org.springframework.jms.config.JmsListenerContainerFactory;
 import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
 import org.springframework.jms.support.converter.MessageConverter;
 import org.springframework.jms.support.converter.MessageType;
+import org.springframework.util.ErrorHandler;
 
 import javax.jms.ConnectionFactory;
 
 @Configuration
 @EnableJms
+@Slf4j
 public class JmsConfiguration {
     @Bean
     public JmsListenerContainerFactory<?> myFactory(ConnectionFactory connectionFactory,
                                                     DefaultJmsListenerContainerFactoryConfigurer configurer) {
-        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
-        // This provides all boot's default to this factory, including the message converter
+        final DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+        factory.setErrorHandler(errorHandler());
         configurer.configure(factory, connectionFactory);
-        // You could still override some of Boot's default if necessary.
         return factory;
+    }
+
+    @Bean
+    public ErrorHandler errorHandler() {
+        return throwable -> {
+            log.warn("In default jms error handler...");
+            log.error("Error Message : {}", throwable.getMessage());
+        };
     }
 
     @Bean // Serialize message content to json using TextMessage
@@ -33,8 +43,6 @@ public class JmsConfiguration {
         MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
         converter.setTargetType(MessageType.TEXT);
         converter.setTypeIdPropertyName("_type");
-        //converter.setObjectMapper(new ObjectMapper()
-          //      .configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false));
         return converter;
     }
 }
