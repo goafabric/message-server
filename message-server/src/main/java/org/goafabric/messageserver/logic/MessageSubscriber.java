efec9c53748700462784ff504e0379cfc867bdd8
Mautsch, Andreas <andreas.mautsch@cgm.com>
2020-06-11 11:28:31 +0200

    refactoring

--- message-server/src/main/java/org/goafabric/messageserver/logic/MessageSubscriber.java
+++ message-server/src/main/java/org/goafabric/messageserver/logic/MessageSubscriber.java
@@ -2,7 +2,6 @@ package org.goafabric.messageserver.logic;
 
 import lombok.extern.slf4j.Slf4j;
 import org.goafabric.messageserver.dto.MyMessage;
-import org.goafabric.messageserver.dto.Person;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
@@ -10,7 +9,7 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class MessageSubscriber {
 
-    @JmsListener(destination = "PERSON_OPEN", containerFactory = "myFactory")
+    @JmsListener(destination = "PERSON_OPEN")
     public void subscribe(MyMessage message) {
         //final Person person = (Person) message.getObject();
         log.info("Received message with topic {} and Person {}", message.getTopic());
