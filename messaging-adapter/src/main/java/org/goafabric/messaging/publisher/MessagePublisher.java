2e21b086a0346545f6412c2726a55a90d863688e
Mautsch, Andreas <andreas.mautsch@cgm.com>
2020-06-14 11:16:42 +0200

    poms refactored

--- messaging-adapter/src/main/java/org/goafabric/messaging/publisher/MessagePublisher.java
+++
@@ -1,8 +0,0 @@
-package org.goafabric.messaging.publisher;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public interface MessagePublisher {
-    void publish(EventMessage message);
-}
