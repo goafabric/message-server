4a3a4bfdd6595e9c044a327aed9e7e573d487e92
Mautsch, Andreas <andreas.mautsch@cgm.com>
2020-06-14 10:38:30 +0200

    objects added

--- message-adapter/src/main/java/org/goafabric/messageserver/publisher/EventMessage.java
+++ message-adapter/src/main/java/org/goafabric/messageserver/publisher/EventMessage.java
@@ -1,14 +1,16 @@
 package org.goafabric.messageserver.publisher;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @Data
-@AllArgsConstructor
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class EventMessage implements Serializable {
     private String topic;
     private String referenceId;
--- message-adapter/src/main/java/org/goafabric/messageserver/publisher/Patient.java
+++ message-adapter/src/main/java/org/goafabric/messageserver/publisher/Patient.java
@@ -1,12 +1,16 @@
 package org.goafabric.messageserver.publisher;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
-@AllArgsConstructor
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Patient implements Serializable {
     private String firstName;
     private String lastName;
