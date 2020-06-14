193516b29bbdb2b37b2f0a81cbd44d153730ce08
Mautsch, Andreas <andreas.mautsch@cgm.com>
2020-06-14 10:44:20 +0200

    poms refactored

--- message-client/src/test/java/org/goafabric/messageserver/ClientSendAndReceiveNRIT.java
+++ message-client/src/test/java/org/goafabric/messageserver/ClientSendAndReceiveNRIT.java
@@ -12,7 +12,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest//(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @RunWith(SpringRunner.class)
 @Slf4j
-public class ClientSendAndReceiveIT {
+public class ClientSendAndReceiveNRIT {
     @Autowired
     private MessagePublisher messagePublisher;
 
