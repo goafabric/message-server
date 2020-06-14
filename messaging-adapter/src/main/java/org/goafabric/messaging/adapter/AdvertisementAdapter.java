9ef47662072eee4821001139e9f65b3c56b8118e
Mautsch, Andreas <andreas.mautsch@cgm.com>
2020-06-14 13:51:55 +0200

    rabbitmq

--- messaging-adapter/src/main/java/org/goafabric/messaging/adapter/AdvertisementAdapter.java
+++ messaging-adapter/src/main/java/org/goafabric/messaging/adapter/AdvertisementAdapter.java
@@ -1,30 +1,34 @@
 package org.goafabric.messaging.adapter;
 
 import lombok.extern.slf4j.Slf4j;
+import org.goafabric.messaging.dto.Patient;
 import org.goafabric.messaging.publisher.EventMessage;
 import org.goafabric.messaging.publisher.MessagePublisher;
-import org.goafabric.messaging.dto.Patient;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
-public class AdvertisementJmsAdapter {
+public class AdvertisementAdapter {
     @Autowired
     private MessagePublisher messagePublisher;
 
     @JmsListener(destination = "patient.open")
+    @RabbitListener(queues = "patient.open")
     public void patientOpen(EventMessage message) {
         logMessage(message);
     }
 
     @JmsListener(destination = "patient.close")
+    @RabbitListener(queues = "patient.close")
     public void patientClose(EventMessage message) {
         logMessage(message);
     }
 
     @JmsListener(destination = "prescription.open")
+    @RabbitListener(queues = "prescription.open")
     public void prescriptionOpen(EventMessage message) {
         logMessage(message);
         messagePublisher.publish(new EventMessage("banner.show",
@@ -32,6 +36,7 @@ public class AdvertisementJmsAdapter {
     }
 
     @JmsListener(destination = "prescription.close")
+    @RabbitListener(queues = "prescription.close")
     public void prescriptionClose(EventMessage message) {
         logMessage(message);
     }
--- messaging-client/src/main/java/org/goafabric/messaging/subscriber/BannerSubscriber.java
+++ messaging-client/src/main/java/org/goafabric/messaging/subscriber/BannerSubscriber.java
@@ -2,13 +2,15 @@ package org.goafabric.messaging.subscriber;
 
 import lombok.extern.slf4j.Slf4j;
 import org.goafabric.messaging.publisher.EventMessage;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
-public class BannerJmsSubscriber {
+public class BannerSubscriber {
     @JmsListener(destination = "banner.show")
+    @RabbitListener(queues = "banner.show")
     public void bannerShow(EventMessage message) {
         log.info("Received message with topic {} and id {}"
                 , message.getTopic(), message.getReferenceId());
