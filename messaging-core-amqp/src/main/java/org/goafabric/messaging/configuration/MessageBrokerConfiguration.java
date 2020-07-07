a503664ff18895bce805ec810acf23a74c2c16a4
Mautsch, Andreas <andreas.mautsch@cgm.com>
2020-07-07 13:55:35 +0200

    refactoring

--- messaging-core-amqp/src/main/java/org/goafabric/messaging/configuration/MessageBrokerConfiguration.java
+++ messaging-core-amqp/src/main/java/org/goafabric/messaging/configuration/MessageBrokerConfiguration.java
@@ -1,7 +1,12 @@
 package org.goafabric.messaging.configuration;
 
 import lombok.extern.slf4j.Slf4j;
+import org.goafabric.MessageQueue;
 import org.goafabric.messaging.publisher.MessagePublisher;
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,7 +14,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @Slf4j
 public class MessageBrokerConfiguration {
-    public static final String excludeClass = "org.springframework.boot.autoconfigure.jms.activemq.ActiveMQAutoConfiguration";
+    public static final String EXCLUDE_CLASS = "org.springframework.boot.autoconfigure.jms.activemq.ActiveMQAutoConfiguration";
 
     @Bean
     public MessagePublisher messagePublisher(RabbitTemplate rabbitTemplate) {
@@ -20,20 +25,15 @@ public class MessageBrokerConfiguration {
         };
     }
 
-    /*
     @Bean
-public AmqpAdmin amqpAdmin() {
-    return new RabbitAdmin(connectionFactory);
-}
-Then you add it to your service:
+    public AmqpAdmin amqpAdmin(ConnectionFactory connectionFactory) {
+        final AmqpAdmin amqpAdmin =  new RabbitAdmin(connectionFactory);
 
-@Autowired
-private AmqpAdmin admin;
-Finally you can use it to create queues and bindings.
+        for (String queueName : MessageQueue.getAllQueues()) {
+            Queue queue = new Queue(queueName, Boolean.TRUE, false, false);
+            amqpAdmin.declareQueue(queue);
+        }
+        return amqpAdmin;
+    }
 
-Queue queue = new Queue(queueName, durable, false, false);
-Binding binding = new Binding(queueName, Binding.DestinationType.QUEUE, EXCHANGE, routingKey, null);
-admin.declareQueue(queue);
-admin.declareBinding(binding);
-     */
 }
